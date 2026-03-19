f8f2a682d0f3b3bcc519451af832ec83f7a7c29a
HeKtonk <hoyez.corey@gmail.com>
2026-03-19 15:32:37 +0100

    Adding a script to run the gen + src directory where the gen will be

---
+++ src/textnode.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class TextType(Enum):
+    plain_text = ""
+    bold_text = "**Bold text**"
+    italic_text = "_Italic text_"
+    code_text = "`Code text`"
+    link = "[anchor text](url)"
+    image = "![alt text](url)"
+
+class TextNode:
+    def __init__(self, TEXT, TEXT_TYPE, URL=None):
+        self.text = TEXT
+        self.text_type = TEXT_TYPE
+        self.url = URL
+
+    def __eq__(self, other):
+        return (isinstance(other, TextNode)) and (self.text == other.text) and (self.text_type == other.text_type) and (self.url == other.url)
+
+    def __repr__(self):
+        return f'{type(self).__name__}({self.text}, {self.text_type}, {self.url})'
+    
+
