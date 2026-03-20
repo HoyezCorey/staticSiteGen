4d1d8cc456ecb6d9bda0b82a535ee00a8e62cde7
HeKtonk <hoyez.corey@gmail.com>
2026-03-20 17:09:26 +0100

    Add testunit when nodes are differents

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,10 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_notEq(self):
+        node = TextNode("I'm a text node", TextType.LINK, "[link](https:boot.dev)")
+        node2 = TextNode("I'm another one text node", TextType.IMAGE, "![image](url/to/image.png)")
+        self.assertNotEqual(node, node2)
 
 if __name__ == "__main__":
     unittest.main()
