67f0ce728fd16d3b797b2655ae4f43c80312addc
HeKtonk <hoyez.corey@gmail.com>
2026-03-24 12:16:18 +0100

    Add to_html method to the ParentNode class

--- src/parentnode.py
+++ src/parentnode.py
@@ -3,3 +3,11 @@ from htmlnode import HTMLNode
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
         super().__init__(tag, children, props=props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("Missing tag")
+        if not self.children:
+            raise ValueError("Missing children")
+        else:
+            return f'<{self.tag}>{self.children.to_html()}<{self.tag}/>'
