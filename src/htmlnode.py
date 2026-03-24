363147aef0f8c11c6e5592fa0224489096e52cc0
HeKtonk <hoyez.corey@gmail.com>
2026-03-24 11:17:50 +0100

    update props_to_html method

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -2,17 +2,19 @@ class HTMLNode():
     def __init__(self, tag = None, value = None, children = None, props = None):
         self.tag = tag
         self.value = value
-        self.children = children
-        self.props = props
+        self.children = children if children else []
+        self.props = props if props else {}
 
     def to_html(self):
         raise NotImplementedError
     
     def props_to_html(self):
         props_to_html = ""
+        if self.props is None:
+            return props_to_html
         for prop in self.props:
             props_to_html += f'{prop}="{self.props[prop]}" '
-        return props_to_html
+        return props_to_html.rstrip()
     
     def __eq__(self, other):
         return (isinstance(other, HTMLNode)) and (self.tag == other.tag) and (self.value == other.value) and (self.children == other.children) and (self.props == other.props)
