0f0148d76bab45b7c40792d8c44cb9a423f3ce27
HeKtonk <hoyez.corey@gmail.com>
2026-03-25 14:51:21 +0100

    Add new test for testing multiple grandchild in one parent

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -18,5 +18,15 @@ class TestParentNode(unittest.TestCase):
             "<div><span><b>grandchild</b></span></div>",
         )
 
+    def test_to_html_with_more_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        grandchild_node_i = LeafNode("i", "grandchild_i")
+        child_node = ParentNode("span", [grandchild_node, grandchild_node_i])
+        parent_node = ParentNode("div", [child_node, child_node, grandchild_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b><i>grandchild_i</i></span><span><b>grandchild</b><i>grandchild_i</i></span><b>grandchild</b></div>",
+        )
+
 if __name__ == "__main__":
     unittest.main()
