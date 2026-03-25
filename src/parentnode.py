8f66b2511400ce25b76f835b5dbf63036fcba43b
HeKtonk <hoyez.corey@gmail.com>
2026-03-25 15:22:55 +0100

    Fix if condition in to_html method

--- src/parentnode.py
+++ src/parentnode.py
@@ -6,9 +6,9 @@ class ParentNode(HTMLNode):
         super().__init__(tag, value=None, children=children, props=props)
 
     def to_html(self):
-        if not self.tag:
+        if not self.tag or self.tag is None:
             raise ValueError(f"Missing tag : {self.tag}")
-        if not self.children:
+        if not self.children or self.children is None:
             raise ValueError(f"Missing children : {self.children}")
         else:
             return f'<{self.tag}>{"".join(str(child.to_html()) for child in self.children)}</{self.tag}>'
