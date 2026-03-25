843a999819f9c81c0acda39aca7b1ae982740e11
HeKtonk <hoyez.corey@gmail.com>
2026-03-25 14:34:25 +0100

    Fix to_html method

--- src/leafnode.py
+++ src/leafnode.py
@@ -8,7 +8,7 @@ class LeafNode(HTMLNode):
     def to_html(self):
         if self.value == None:
             raise ValueError
-        if not self.accepted_tag():
+        if not self.tag or self.tag is None:
             return self.value
         else:
             match self.tag:
@@ -18,10 +18,6 @@ class LeafNode(HTMLNode):
                     return f'<{self.tag}{" "+ self.props_to_html() if self.props else ""} />'
                 case _:
                     return f'<{self.tag}>{self.value}</{self.tag}>'
-            
-    def accepted_tag(self):
-        tags = set(item.value for item in TextType)
-        return self.tag in tags
             
     def __repr__(self):
         return f"{self.tag}\n{self.value}\n{self.props_to_html()}"
