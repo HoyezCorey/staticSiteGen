a9bb5074a045c8d155c9c0e04f50256419eaf26e
HeKtonk <hoyez.corey@gmail.com>
2026-03-30 12:35:35 +0200

    Add italic test case

--- src/test_split_delimiter.py
+++ src/test_split_delimiter.py
@@ -11,3 +11,12 @@ class TestSplitDelimiter(unittest.TestCase):
             TextNode("bold", TextType.BOLD),
             TextNode(" word", TextType.TEXT)
         ])
+
+    def test_italic_delimiter_in_text(self):
+        node = TextNode("I'm a textnode with an _italic_ word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node],"_", TextType.TEXT)
+        self.assertEqual(new_nodes, [
+            TextNode("I'm a textnode with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word", TextType.TEXT)
+        ])
